a697d5736ca2e1325d6138ce79ab5bec9c87d28a
Jozef Izso <jozef.izso@gmail.com>
2013-07-08 09:57:25 +0200

    Using latest APR library from jozefizso/homebrew-dupes

--- apache24.rb
+++ apache24.rb
@@ -3,37 +3,27 @@ require 'formula'
 
 class Apache24 < Formula
   homepage 'https://httpd.apache.org/'
-  url 'https://www.apache.org/dist/httpd/httpd-2.4.4.tar.gz'
-  sha1 '2834fa7c0a510890a0a2f946359c6f64d73abf13'
+  url 'http://www.apache.org/dyn/closer.cgi?path=httpd/httpd-2.4.4.tar.bz2'
+  sha1 '0c5ab7f876aa10fbe8bfab2c34f8dd3dc76db16c'
   version '2.4.4'
 
   skip_clean ['bin', 'sbin']
 
   depends_on 'pcre'
-
-  # Apache 2.4 no longer bundles apr or apr-util so we have to fetch
-  # it manually for each build
-  def fetch_apr
-    ["apr-1.4.6", "apr-util-1.4.1"].each do |tb|
-      curl "-s", "-o", "#{tb}.tar.gz", "https://www.apache.org/dist/apr/#{tb}.tar.gz"
-      system "tar -xzf #{tb}.tar.gz"
-      dir = tb.reverse.split('-', 2).map(&:reverse).reverse[0]
-      FileUtils.mv(tb, "srclib/#{dir}")
-    end
-  end
+  depends_on 'jozefizso/dupes/apr'
+  depends_on 'jozefizso/dupes/apr-util'
 
   def install
-    fetch_apr
-
     args = [
       "--prefix=#{prefix}",
       "--mandir=#{man}",
       "--disable-debug",
       "--disable-dependency-tracking",
       "--with-mpm=prefork",
-      "--with-included-apr",
       "--enable-mods-shared=all",
       "--with-pcre=#{Formula.factory('pcre').prefix}",
+      "--with-apr=#{Formula.factory('jozefizso/dupes/apr').prefix}",
+      "--with-apr-util=#{Formula.factory('jozefizso/dupes/apr-util').prefix}",
     ]
     system './configure', *args
     system "make"
